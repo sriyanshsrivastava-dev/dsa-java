6895ced1133cd499983a84669dd2bbd51d2e4554
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-03-18 13:21:49 +0530

    chore: add Recursion logic, concept, application and test cases with factorial example.

---
+++ src/main/java/dsa/playground/Playground.java
@@ -0,0 +1,11 @@
+package dsa.playground;
+
+import dsa.recursion.RecursionConcept;
+
+public class Playground {
+    public static void main(String[] args) {
+
+        int recursionResult = RecursionConcept.fact(4);
+        System.out.println(recursionResult);
+    }
+}

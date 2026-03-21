02a0cc1bc1aede523ad921c36c64877960b926a7
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-03-21 21:35:03 +0530

    chore: add Edgecase test for factorial

--- src/test/java/dsa/recursion/RecursionConceptTest.java
+++ src/test/java/dsa/recursion/RecursionConceptTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RecursionConceptTest {
 
     @Test
-    void testFact(){
+    void testFactorial() {
         assertEquals(24, RecursionConcept.fact(4));
-        assertEquals(1, RecursionConcept.fact(1));
-        assertEquals(1, RecursionConcept.fact(0));
-        assertEquals(120, RecursionConcept.fact(5));
-        assertEquals(6, RecursionConcept.fact(3));
     }
+
+    @Test
+    void testFactorialEdgeCase(){
+        assertEquals(1,RecursionConcept.fact(0));
+        assertEquals(1,RecursionConcept.fact(1));
+
+    }
+
 }

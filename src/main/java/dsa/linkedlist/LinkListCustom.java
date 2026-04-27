ca593af5d00a89836373d202257d1dd622147b3d
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-04-27 20:28:46 +0530

    feat: add value to the front of linkedlist, print list using display() method.

--- src/main/java/dsa/linkedlist/LinkListCustom.java
+++ src/main/java/dsa/linkedlist/LinkListCustom.java
@@ -4,8 +4,8 @@ public class LinkListCustom {
 
     // Variables and internal Classes
 
-    private int head;
-    private int tail;  // we track tail to make linklist of O(1).
+    private Node head;
+    private Node tail;  // we track tail to make linklist of O(1).
     private int size;
 
 
@@ -18,19 +18,20 @@ public class LinkListCustom {
             this.value = value;
         }
 
-        public Node(int value,Node next){
+        public Node(int value, Node next){
             this.value = value;
             this.next = next;
         }
         
     }
+
     // Constructors
 
     public LinkListCustom(){
-        head = 0;
         size = 0;
     }
 
+    
     // Get LinkedList size
     public int getSize(){
         return this.size;
@@ -38,11 +39,43 @@ public class LinkListCustom {
 
 
     // Insert Front
+    public void addFront(int value){
+
+        Node node = new Node(value);
+
+        if(head == null){
+            head = node;
+            tail = node;
+            size++;
+            return;
+        }
+
+        node.next = head;
+        head = node;
+        size++;
+    }
+
     // Insert Last
     // Insert at Nth position
     // Delete Front
     // Delete Last
     // Delete Nth position
     // Print Linklist
+    public void display(){
+
+        if(head == null){
+            System.out.println("List is empty.");
+            return;
+        }
+
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("END");
+    }
 
 }
--- src/main/java/dsa/linkedlist/LinkedListMain.java
+++ src/main/java/dsa/linkedlist/LinkedListMain.java
@@ -4,6 +4,12 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Start Linked List Data Structure and Algorithms.");
         LinkListCustom linklist = new LinkListCustom();
+        LinkListCustom linklist2 = new LinkListCustom();
+        linklist.addFront(10);
+        linklist.addFront(20);
+        linklist.addFront(50);
         System.out.println(linklist.getSize());
+        linklist.display();
+        linklist2.display();
     }
 }

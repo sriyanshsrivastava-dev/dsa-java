b07045d99865dd3eb278aa6ca01061b895bcfabb
Sriyansh Srivastava <sriyanshsrivastava.dev@gmail.com>
2026-04-30 14:57:22 +0530

    feat: add instance variables, Node class for doubly linked list, accessor, method to check if list is empty, insertFront and insertBack methods.

--- src/main/java/dsa/linkedlist/DoublyLinkedList.java
+++ src/main/java/dsa/linkedlist/DoublyLinkedList.java
@@ -1,10 +1,121 @@
 package dsa.linkedlist;
 
+import java.util.NoSuchElementException;
+
+
+
 public class DoublyLinkedList {
 
     // class variables
+    private Node head;
+    private Node tail;
+    private int size;
+
     // Private class Node
+    private class Node{
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node(int value){
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+
+        }
+
+        public Node(int value, Node nextNode, Node prevNode){
+            this.value = value;
+            this.next = nextNode;
+            this.prev = prevNode;
+        }
+    }
+
     // constructors
+
+    public DoublyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    // accessor
+    public boolean isEmpty(){
+        if (head == null && tail == null){
+            return true;
+        }
+        return false;
+    }
+
+    public int getHead(){
+        if (this.isEmpty()){
+            throw new NoSuchElementException("Linked list is empty, Head is null.");
+        }
+        return this.head.value;
+    }
+
+    public int getTail(){
+        if (this.isEmpty()){
+            throw new NoSuchElementException("Linked list is empty, Tail is null.");
+        }
+        return this.tail.value;
+    }
+
     // operations
+
+    // get size
+    public int getSize(){
+        return this.size;
+    }
+
+    // Insert at Front
+    public void insertFront(int value){
+
+        Node node = new Node(value);
+
+        if(head == null){
+            head = node;
+            tail = node;
+            size++;
+            return;
+        }
+
+        node.next = head;
+        head.prev = node;
+        head = node;
+        size++;
+    }
+
+    // Insert at Back
+    public void insertBack(int value){
+
+        if(head == null && tail == null){
+            this.insertFront(value);
+            return;
+        }
+
+        Node node = new Node(value);
+
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        size++;
+    }
+
+    // display the linklist
+    public void display(){
+
+        if(head == null){
+            System.out.println("Empty List");
+            return;
+        }
+
+        Node tempNode = head;
+
+        while(tempNode != null){
+            System.out.print(tempNode.value + " <-> ");
+            tempNode = tempNode.next;
+        }
+        System.out.println("END");
+    }
 
 }
--- src/main/java/dsa/linkedlist/DoublyLinkedListMain.java
+++ src/main/java/dsa/linkedlist/DoublyLinkedListMain.java
@@ -3,5 +3,18 @@ package dsa.linkedlist;
 public class DoublyLinkedListMain {
     public static void main(String[] args) {
         System.out.println("Doubly Linked List");
+
+        DoublyLinkedList doublyLinkList = new DoublyLinkedList();
+        doublyLinkList.display();
+
+//        doublyLinkList.insertFront(10);
+        doublyLinkList.insertBack(50);
+        doublyLinkList.insertBack(100);
+        System.out.println(doublyLinkList.isEmpty());
+//        System.out.println(doublyLinkList.getHead());
+        System.out.println("Head is: "+ doublyLinkList.getHead());
+        System.out.println("Tail is: "+ doublyLinkList.getTail());
+        doublyLinkList.display();
+        System.out.println("Size: " + doublyLinkList.getSize());
     }
 }
